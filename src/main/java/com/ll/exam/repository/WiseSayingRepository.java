7275934a60099f7fabfc0f870ba0550a168f4545
ahah525 <ahah525@naver.com>
2022-07-13 20:36:26 +0900

    WiseSayingRepository 도입

---
+++ src/main/java/com/ll/exam/repository/WiseSayingRepository.java
@@ -0,0 +1,46 @@
+package com.ll.exam.repository;
+
+import com.ll.exam.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WiseSayingRepository {
+    private int wiseSayingLastId;   // 가장 마지막으로 추가된 명언 글 번호
+    private List<WiseSaying> wiseSayings;   // 명언 리스트
+
+    public WiseSayingRepository() {
+        wiseSayingLastId = 0;
+        wiseSayings = new ArrayList<>();
+    }
+
+    public WiseSaying write(String content, String author) {
+        int id = ++wiseSayingLastId;
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
+        return wiseSaying;
+    }
+
+    public WiseSaying findById(int id) {
+        for (WiseSaying ws : wiseSayings) {
+            if (ws.getId() == id)
+                return ws;
+        }
+        return null;
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings;
+    }
+
+    public void modify(int id, String content, String author) {
+        WiseSaying wiseSaying = findById(id);
+        wiseSaying.setContent(content);
+        wiseSaying.setAuthor(author);
+    }
+
+    public void remove(int id) {
+        WiseSaying wiseSaying = findById(id);
+        wiseSayings.remove(wiseSaying);
+    }
+}
--- src/main/java/com/ll/exam/service/WiseSayingService.java
+++ src/main/java/com/ll/exam/service/WiseSayingService.java
@@ -1,51 +1,40 @@
 package com.ll.exam.service;
 
 import com.ll.exam.WiseSaying;
+import com.ll.exam.repository.WiseSayingRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WiseSayingService {
-    private int wiseSayingLastId;   // 가장 마지막으로 추가된 명언 글 번호
-    private List<WiseSaying> wiseSayings;   // 명언 리스트
+    private WiseSayingRepository wiseSayingRepository;
 
     public WiseSayingService() {
-        wiseSayingLastId = 0;
-        wiseSayings = new ArrayList<>();
+        wiseSayingRepository = new WiseSayingRepository();
     }
 
     // 명언 저장
     public WiseSaying write(String content, String author) {
-        int id = ++wiseSayingLastId;
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-        wiseSayings.add(wiseSaying);
-        return wiseSaying;
+        // 명언 메모리에 저장
+        return wiseSayingRepository.write(content, author);
     }
 
     // id로 명언 조회
     public WiseSaying findById(int id) {
-        for (WiseSaying ws : wiseSayings) {
-            if (ws.getId() == id)
-                return ws;
-        }
-        return null;
+        return wiseSayingRepository.findById(id);
     }
 
     // 모든 명언 조회
     public List<WiseSaying> findAll() {
-        return wiseSayings;
+        return wiseSayingRepository.findAll();
     }
 
     // id로 명언 수정
     public void modify(int id, String content, String author) {
-        WiseSaying wiseSaying = findById(id);
-        wiseSaying.setContent(content);
-        wiseSaying.setAuthor(author);
+        wiseSayingRepository.modify(id, content, author);
     }
 
     // id로 명언 삭제
     public void remove(int id) {
-        WiseSaying wiseSaying = findById(id);
-        wiseSayings.remove(wiseSaying);
+        wiseSayingRepository.remove(id);
     }
 }
