dfdc79c786e5b390b11595d5b5977ae7472a57f5
ahah525 <ahah525@naver.com>
2022-07-13 17:51:13 +0900

    App의 일부 기능 WiseSayingController로 분리

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,22 +1,21 @@
 package com.ll.exam;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.ll.exam.controller.WiseSayingController;
+
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
-    private int wiseSayingLastId;   // 가장 마지막으로 추가된 명언 글 번호
-    private List<WiseSaying> wiseSayings;   // 명언 리스트
+    private WiseSayingController wiseSayingController;
+
     public App(Scanner sc) {
         this.sc = sc;
-        wiseSayingLastId = 0;
-        wiseSayings = new ArrayList<>();
     }
 
     // 실행 메서드
     public void run() {
         System.out.println("== 명언 SSG ==");
+        wiseSayingController = new WiseSayingController(sc);  // 컨트롤러 생성
 
         outer:
         while (true) {
@@ -26,82 +25,19 @@ public class App {
 
             switch (rq.getPath()) {
                 case "등록":
-                    int id = ++wiseSayingLastId;
-                    System.out.println("명언 : ");
-                    String content = sc.nextLine();
-                    System.out.println("작가 : ");
-                    String author = sc.nextLine();
-                    // 리스트에 WiseSaying 인스턴스 삽입
-                    wiseSayings.add(new WiseSaying(id, content, author));
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    wiseSayingController.write(rq);
                     break;
                 case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-                    // 명언 최신순(역순) 출력
-                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-                        WiseSaying ws = wiseSayings.get(i);
-                        System.out.printf("%d / %s / %s\n", ws.getId(), ws.getAuthor(), ws.getContent());
-                    }
-                    break;
+                    wiseSayingController.list(rq);
                 case "삭제":
-                    remove(rq);
+                    wiseSayingController.remove(rq);
                     break;
                 case "수정":
-                    modify(rq);
+                    wiseSayingController.modify(rq);
                     break;
                 case "종료":
                     break outer;    // outer 라벨이 붙은 곳에서 break
             }
         }
-    }
-    public void remove(Rq rq) {
-        int id = rq.getIntParam("id", 0);
-        if (id == 0) {
-            System.out.println("번호를 입력해주세요.");
-            return;
-        }
-        WiseSaying foundWs = findById(id);  // id로 조회한 명언
-        // 해당 id에 대한 명언이 존재하지 않는 경우 예외 처리
-        if (foundWs == null) {
-            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
-            return;
-        }
-        // 명언 삭제
-        wiseSayings.remove(foundWs);
-        System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
-    }
-
-    public void modify(Rq rq) {
-        int id = rq.getIntParam("id", 0);
-        if (id == 0) {
-            System.out.println("번호를 입력해주세요.");
-            return;
-        }
-        WiseSaying foundWs = findById(id);
-        if (foundWs == null) {
-            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
-            return;
-        }
-        // 새로 명언, 작가 입력받기
-        System.out.printf("명언(기존) : %s", foundWs.getContent());
-        System.out.print("명언 : ");
-        String content = sc.nextLine();
-        System.out.printf("작가(기존) : %s", foundWs.getAuthor());
-        System.out.print("명언 : ");
-        String author = sc.nextLine();
-        // 명언 수정
-        foundWs.setContent(content);
-        foundWs.setAuthor(author);
-        System.out.printf("%d번 명언이 수정되었습니다.\n", id);
-    }
-
-    // id로 명언 조회
-    public WiseSaying findById(int id) {
-        for (WiseSaying ws : wiseSayings) {
-            if(ws.getId() == id)
-                return ws;
-        }
-        return null;
     }
 }
