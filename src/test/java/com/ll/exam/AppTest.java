bac1f193b35261cb9701bec342f9e9b290af3ad1
ahah525 <ahah525@naver.com>
2022-07-13 14:43:30 +0900

    테스트를 위한 입출력 리다이렉팅

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,6 +2,9 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,5 +13,23 @@ public class AppTest {
     public void 테스트() {
         assertTrue(1 == 1);
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void 스캐너에_키보드가_아닌_문자열을_입력으로_설정() {
+        Scanner sc = TestUtil.genScanner("안녕");
+        String cmd = sc.nextLine().trim();// 한줄 읽어서 앞뒤 공백 제거
+        // "안녕"이 스캐너에 입력된 문자열이 맞는지 검증
+        assertEquals("안녕", cmd);
+    }
+
+    @Test
+    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
+        // 모니터에 출력할 문자열이 모두 output에 담김
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.println("안녕");
+        String rs = output.toString();
+        // output에 담긴 문자열이 "안녕"이 맞는지 검증
+        assertEquals("안녕", rs);
     }
 }
