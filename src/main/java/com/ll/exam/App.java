e140716388d4f2224e2dd5cc5d0b747b00c8d600
ahah525 <ahah525@naver.com>
2022-07-13 16:01:44 +0900

    등록시 명언과 작가를 입력받는지 테스트

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -15,9 +15,15 @@ public class App {
         outer:
         while (true) {
             System.out.println("명령) ");
-            String cmd = sc.nextLine();
+            String cmd = sc.nextLine().trim();  // 한 줄 입력받고 앞뒤 공백 제거
 
             switch (cmd) {
+                case "등록":
+                    System.out.println("명언 : ");
+                    sc.nextLine();
+                    System.out.println("작가 : ");
+                    sc.nextLine();
+                    break;
                 case "종료":
                     break outer;    // outer 라벨이 붙은 곳에서 break
             }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -10,18 +10,37 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
-        public void 프로그램_시작시_타이틀_츨력_그리고_종료() {
-            Scanner sc = TestUtil.genScanner("종료");
-            ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+    public void 프로그램_시작시_타이틀_츨력_그리고_종료() {
+        Scanner sc = TestUtil.genScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
 
-            new App(sc).run();  // 실행
+        new App(sc).run();  // 실행
 
-            String rs = output.toString();
-            TestUtil.clearSetOutToByteArray(output);
-            // 프로그램 시작 후, 종료 입력시 출력되어야할 결과가 맞는지 검증
-            assertTrue(rs.contains("== 명언 SSG =="));
-            assertTrue(rs.contains("명령) "));
-        }
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+        // 프로그램 시작 후, 종료 입력시 출력되어야할 결과가 맞는지 검증
+        assertTrue(rs.contains("== 명언 SSG =="));
+        assertTrue(rs.contains("명령) "));
+    }
+
+    @Test
+    public void 등록을_하면_명언과_작가를_물어본다() {
+        Scanner sc = TestUtil.genScanner("""
+                등록
+                나의 죽음을 적들에게 알리지 말라
+                이순신
+                종료
+                """);
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+        // 등록 입력시 명언, 작가가 나오는지 검증
+        assertTrue(rs.contains("명언 : "));
+        assertTrue(rs.contains("작가 : "));
+    }
 
     @Test
     public void 테스트() {
